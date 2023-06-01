9118942137525ce266fe08b73d75bcc5e76d475d
laurennhong <sejinhong.lauren@gmail.com>
2023-06-01 11:37:34 -0700

    usability test

--- capstone/development/usability/script.js
+++ capstone/development/usability/script.js
@@ -6,6 +6,7 @@
     const homeicon = document.querySelector(".homeicon");
     const cow = document.querySelector('#cow');
     const next = document.querySelector('#next');
+    const back = document.querySelector('.back');
     cow.addEventListener('click', function(event){
         document.querySelector('#homepage').className = 'hidden';
         document.querySelector('#cowpage1').className = 'showing';
@@ -14,11 +15,18 @@
             document.querySelector('#cowpage1').className = 'hidden';
             document.querySelector('#cowpage2').className = 'showing';
         })
-    })
 
-    homeicon.addEventListener('click', function() {
-        homepage.className = 'showing';
-        document.querySelector('#cowpage1').className = 'hidden';  
+        homeicon.addEventListener('click', function() {
+            // document.querySelector('.cowsection').className = 'hidden'; 
+            // document.querySelector('#cowpage2').className = 'hidden'; 
+            document.querySelector('#cowpage1').className = 'hidden'; 
+            homepage.className = 'showing';
+        })
+
+        back.addEventListener('click', function() {
+            document.querySelector('#cowpage2').className = 'hidden'; 
+            document.querySelector('#cowpage1').className = 'showing'; 
+        })
     })
 
     alert(`USER SCENARIO: “You’re a 3rd grade student living in Davis, CA learning about plants and animals in class. One day in class your teacher share this website with you where you can learn about animals and plants in Davis. You open the site and see all the options available to you. You choose the cow which introduces you to new content. You can navigate back to the home page.”
