f4b73f4bd59e217fb6672677c01780b89af3ce99
laurennhong <sejinhong.lauren@gmail.com>
2023-06-06 23:09:38 -0700

    usability

--- capstone/development/usability/script.js
+++ capstone/development/usability/script.js
@@ -29,12 +29,7 @@
         })
     })
 
-    alert(`USER SCENARIO: “You’re a 3rd grade student living in Davis, CA learning about plants and animals in class. One day in class your teacher share this website with you where you can learn about animals and plants in Davis. You open the site and see all the options available to you. You choose the cow which introduces you to new content. You can navigate back to the home page.”
+    alert(`USER SCENARIO: “You’re a 3rd grade student living in Davis, CA learning about plants and animals in class. One day in class your teacher shares this website with you where you can learn about animals and plants in Davis. You open the site and see all the options available to you. You choose the cow which introduces you to new content. You can navigate back to the home page.”
     
     IN-PROGRESS WARNING: Other buttons besides the cow, are not currently clickable.`);
-
-    console.log(`What do you think works well in terms of readability and visual design and what could be improved on?
-    What was your favorite part of the site?
-    What other type of content about plants and animals would make the site more interesting?
-    `);
 })();
